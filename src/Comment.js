e7a7850ab3859e95fba9b5329697b1b45ad8c02c
Michał Gacek <gacek.michael@outlook.com>
2018-04-30 11:07:01 +0200

    Fixed errors

--- src/Comment.js
+++ src/Comment.js
@@ -1,14 +1,18 @@
-import {connect} from 'react-redux';
-import Comment from './Comment';
-import {thumbUpComment} from './index'
+import React from 'react';
+import { connect } from 'react-redux';
+import { thumbUpComment } from './actions'
 
 const mapDispatchToProps = dispatch => ({
   thumbUpComment: (id) => dispatch(thumbUpComment(id))
 });
 
-const Comment = ({text, votes, id, thumbUpComment}) =>
-<li>
-    {text} <span>votes: {votes}</span> <button onClick={() => thumbUpComment(id)}>Thumb up</button></li>;
+const Comment = ({text, votes, id, thumbUpComment}) => (
+    <li>
+        {text} 
+        <span>votes: {votes}</span> 
+        <button onClick={() => thumbUpComment(id)}>Thumb up</button>
+    </li>
+);
 
 
 export default connect(null, mapDispatchToProps)(Comment);
